34bd9e4a7a38f4ccd26646ab34cb45eb4392ac6e
TMA/HungTran-Win10 <tranmanhhung@tma.com.vn>
2018-08-31 18:05:11 +0700

    draft score.js

--- dom/score-keeper/score.js
+++ dom/score-keeper/score.js
@@ -1,3 +1,19 @@
 var playerOne = 0;
 var playerTwo = 0;
 var isReset = false;
+
+var maxScore = 5;
+
+var h1 = document.querySelector("h1");
+var p = document.querySelector("p");
+
+function modifyCurrentScore(){
+	h1.textContent = playerOne + " to " + playerTwo;
+}
+
+function modifyMaxScore(){
+	p.textContent = "Playing to: " + maxScore;
+}
+
+modifyCurrentScore();
+modifyMaxScore();
