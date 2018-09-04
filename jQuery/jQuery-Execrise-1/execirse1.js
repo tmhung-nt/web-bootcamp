5d6b9fc48ee8fdbcdb8b38c3b7e52a3db934066d
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-09-04 20:49:14 +0700

    #jQuery: basic stuff

---
+++ jQuery/jQuery-Execrise-1/execirse1.js
@@ -0,0 +1,8 @@
+$("div").css("background", "purple");
+
+$("div.highlight").css("width", "200px");
+
+$("div#third").css("border", "solid orange");
+
+// $("div:nth-of-type(1)").css("font-color", "pink");
+$("div:first-of-type").css("font-color", "pink");
