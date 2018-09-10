47543814da743acce7bed7d52730040f099e728a
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-09-10 22:24:30 +0700

    #introToExpress: assignment

---
+++ nodeJs/introToExpress/assignment/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res){
+	res.send("Hi there, welcome to my assignment!");
+});
+
+app.get("/speak/:animal/", function(req, res){
+	var sound = "undefined";
+	var animal = req.params.animal;
+	if (animal === "pig"){
+		sound = "Oink";
+	} else if (animal === "cow"){
+		sound = "Moo";
+	} else if (animal === "dog"){
+		sound = "Woof Woof!";
+	}
+	res.send("The " + animal + " says \"" + sound + "\"");
+});
+
+app.get("/repeat/:verb/:num", function(req, res){
+	var verb = req.params.verb;
+	var num = req.params.num;
+	var str = "";
+	for (var i = 0; i < num; i++){
+		str += verb + " ";
+	}
+	res.send(str);
+
+});
+
+app.get("*", function(req, res){
+	res.send("Sorry, page not found...What are you doing with your life?")
+});
+
+// Tell Express to listen for requests (start server)
+app.listen("3000", function(){
+    console.log("Server has started!!!")
+});
