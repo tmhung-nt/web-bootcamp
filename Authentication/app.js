c338f41d7fe18e65e6ca62246ed22957ee9d1376
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-10-11 06:22:47 +0700

    #authentication: able to login, logout and delete session after logging out

--- Authentication/app.js
+++ Authentication/app.js
@@ -4,32 +4,86 @@ var express                 = require('express'),
     passport                = require('passport'),
     LocalStrategy           = require('passport-local'),
     passportLocalStrategy   = require('passport-local-mongoose'),
-    app                     = express(),
     User                    = require('./models/user');
 
-mongoose.connect("mongodb://locahost/authentication-demo");
+mongoose.connect("mongodb://localhost:27017/authenticationDemo", { useNewUrlParser: true });
+var app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 // to use passpost in Express framework
 app.use(require("express-session")({
-    secret: "wataruS",
+    secret: "first authentication lesson",
     resave: false,
     saveUninitialized: false
 }));
+
+// setup passport to use
+passport.use(new LocalStrategy(User.authenticate()));
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// ==============RestFul ROUTEs
+
+
+//==============================================
+//ROUTES
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 });
+
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//hanlding user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+//LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+//login logic - middle ware before the final callback is run
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"}), function(req, res){
+    ;
+});
+
+//LOGOUT ROUTES
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
+//==============================================
 
 app.listen(3000, function(){
     console.log("server has started!!!");
--- Authentication/readme.md
+++ Authentication/readme.md
@@ -9,8 +9,24 @@
 * Discuss sessions
     * Express-Session
 
-#Auth Code Along Part1
+#Auth CodeAlong Part1
 * Set up folder structure
 * Install needed packages
 * Add root route and template
 * Add secret route and template
+
+#Auth CodeAlong Part2
+* Create User model
+* Configure passport
+
+#Auth CodeAlong Part3
+* Add Register routes
+* Add Register form
+
+#Auth CodeAlong Part4
+* Add Login routes
+* Add Login form
+
+#Auth CodeAlong Part5
+* Add Logout routes
+* Add isLoggedIn middleware
