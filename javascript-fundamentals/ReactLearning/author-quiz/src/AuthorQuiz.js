45106e53878d323a65d43de164c25c6915daa3ec
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-11-13 08:11:32 +0700

    #react: adding authorQuiz project

---
+++ javascript-fundamentals/ReactLearning/author-quiz/src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './AuthorQuiz.css';
+import './bootstrap.min.css';
+
+class AuthorQuiz extends React.Component {
+  render() {
+    return (
+      <div>Author Quiz</div>
+    );
+  }
+}
+
+export default AuthorQuiz;
