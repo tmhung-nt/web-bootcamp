417460752dc6c2878c7dd45f19c8855e3904b196
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-11-23 05:28:56 +0700

    #redux: hello-redux - creating redux store to keep the app's state

--- javascript-fundamentals/ReactLearning/redux-execirses/hello-redux/src/App.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/hello-redux/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
 
 class App extends Component {
   // remove state out of App component as we are going to let Redux manages it
