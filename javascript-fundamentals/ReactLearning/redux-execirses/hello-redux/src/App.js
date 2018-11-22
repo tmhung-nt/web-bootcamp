c3952c5c890091ee3129fc695d74a6c9d998a36c
TMA/HungTran-Dell <tmhung.nt@gmail.com>
2018-11-23 05:42:11 +0700

    #redux: hello-redux - pass reducer and initalState when calling createStore()

--- javascript-fundamentals/ReactLearning/redux-execirses/hello-redux/src/App.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/hello-redux/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 import { createStore } from 'redux';
 
-const store = createStore(reducer);
+const initialState = { tech: 'React'};
+const store = createStore(reducer, initialState);
 
 class App extends Component {
   // remove state out of App component as we are going to let Redux manages it
