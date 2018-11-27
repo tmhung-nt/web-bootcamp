d03e8fc20b2ee55f936e52a6ba6ff1f94ac7ecfd
HungTran <tmhung.nt@gmail.com>
2018-11-28 05:50:51 +0700

    #skypey: adding redux devtools for debugging

--- javascript-fundamentals/ReactLearning/redux-execirses/skypey/src/store/index.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/skypey/src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import reducer from '../reducers';
 
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+// https://codeburst.io/redux-devtools-for-dummies-74566c597d7
+
+const store = createStore(reducer, devToolsEnhancer());
 export default store;
