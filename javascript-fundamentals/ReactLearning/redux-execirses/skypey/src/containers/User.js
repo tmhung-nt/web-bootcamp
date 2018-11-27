0a07790abd2900bd247e3c0084941193a20fc0a5
HungTran <tmhung.nt@gmail.com>
2018-11-28 06:20:39 +0700

    #skypey: get to understand bind() usage in React component

--- javascript-fundamentals/ReactLearning/redux-execirses/skypey/src/containers/User.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/skypey/src/containers/User.js
@@ -4,8 +4,12 @@ import store from '../store';
 import { setActiveUserId } from '../actions';
 
 const User = ({email, name, profile_pic, status, user_id}) => {
+    const handleUserClick = (user_id) => {
+        store.dispatch(setActiveUserId(user_id));
+    }
     return (
-        <div className="User" onClick={handleUserClick.bind(null, user_id)}>
+        // using bind here just to pass user_id when calling the handleUserClick function
+        <div className="User" onClick={handleUserClick.bind(null, user_id)}>  
             <img src={profile_pic} alt={name} className="User__pic" />
             <div className="User__details">
                 <p className="User__details-name">{name}</p>
@@ -15,8 +19,9 @@ const User = ({email, name, profile_pic, status, user_id}) => {
     )
 }
 
-const handleUserClick = (userId) => {
-    store.dispatch(setActiveUserId(userId));
-}
+//// same as when define inside the User component
+// const handleUserClick = (userId) => {
+//     store.dispatch(setActiveUserId(userId));
+// }
 
 export default User;
