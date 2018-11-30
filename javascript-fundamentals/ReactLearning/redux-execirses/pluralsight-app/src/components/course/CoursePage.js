947a8320945c6d8abbf910ce35f333806c8bc2fe
HungTran-PC <tranmanhhung@tma.com.vn>
2018-11-30 17:15:17 +0700

    #pluralsight-app: using connect() in coursePage without providing mapDispatchToProps function

--- javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/course/CoursePage.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/course/CoursePage.js
@@ -1,13 +1,65 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { createCourse } from '../../actions/courseActions';
 class CoursesPage extends React.Component {
+  state = {
+      course: {
+          title: ''
+      }
+  }
+
+  onTitleChange = e => {
+      const course = this.state.course;
+      course.title = e.target.value;
+    this.setState({ course });
+  }
+
+  onClickSave = () => {
+      this.props.dispatch(createCourse(this.state.course));
+      this.setState({course: {
+          title: ''
+      }});
+  }
+
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <div>
+            {this.props.courses.map((course, id) => <p key={id}>{course.title}</p>)}
+        </div>
+        <input 
+            type="text"
+            onChange={this.onTitleChange} 
+            name="title"
+            value={this.state.course.title}
+            placeholder="Course's title goes here"
+        />
+        <input 
+            type="submit"
+            value="Save"
+            onClick={this.onClickSave}
+        />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.PropTypes = {
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => {
+    return {
+        courses: state.courses
+    }
+}
+
+
+
+
+export default connect(mapStateToProps)(CoursesPage);
