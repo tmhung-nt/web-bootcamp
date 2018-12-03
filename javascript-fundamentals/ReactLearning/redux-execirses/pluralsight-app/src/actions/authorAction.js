18394148582fff1c5b42f02f103cd8599da23049
HungTran-PC <tranmanhhung@tma.com.vn>
2018-12-03 13:44:03 +0700

    modify to extract author list from fetchAllCourses call

--- javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/actions/authorAction.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/actions/authorAction.js
@@ -1,11 +1,18 @@
 import * as types from "../constants/action-types";
 import axios from 'axios';
 
+let authors;
 export const loadAuthors = () => {
     return dispatch => {
-        return axios.get(types.FETCH_AUTHORS_URL)
-                    .then(response => dispatch(loadAuthorsSuccess(response.data)))
+        return axios.get(types.FETCH_COURSES_URL) // get courses first then parse author list from there
+                    .then(response => {
+                        authors = response.data.map((course, index ) => ({firstName: course.authorId, id: index}));
+                        dispatch(loadAuthorsSuccess(authors));
+                    })
                     .catch(error => console.log(error));
+        // return AuthorApi.getAllAuthors()
+        //             .then(response => dispatch(loadAuthorsSuccess(response)))
+        //             .catch(error => console.log(error));
     }
 }
 
