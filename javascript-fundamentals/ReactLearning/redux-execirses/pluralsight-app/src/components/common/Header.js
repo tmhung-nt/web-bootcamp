39e73bdea343a6372d489840f448ba02bb3b9163
HungTran-PC <tranmanhhung@tma.com.vn>
2018-12-03 14:15:45 +0700

    create preloader component and add it to Header component

--- javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/common/Header.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
     return (
@@ -9,6 +10,7 @@ const Header = () => {
         <NavLink to="/courses">Courses</NavLink>
         {" | "}
         <NavLink to="/about">About</NavLink>
+        <LoadingDots interval={100} dots={20} />
       </nav>
     );
   };
