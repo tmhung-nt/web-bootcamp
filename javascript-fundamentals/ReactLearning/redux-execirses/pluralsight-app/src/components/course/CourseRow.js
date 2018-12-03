5abf0b9722d15950c61bda5e7faba8e4ec9f6429
HungTran-PC <tranmanhhung@tma.com.vn>
2018-12-03 13:46:19 +0700

    update status via componentWillReceiveProps

--- javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/course/CourseRow.js
+++ javascript-fundamentals/ReactLearning/redux-execirses/pluralsight-app/src/components/course/CourseRow.js
@@ -3,9 +3,10 @@ import propTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import { convertSecondsToMinutes } from '../../services/utils';
 
-const Course = ({course}) => {
+const Course = ({course, index}) => {
     return (
         <tr>
+            <td>{index + 1}</td>
             <td><a href={course.watchHref} target="blank">Watch</a></td>
             <td><Link to={`/course/` + course.id}>{course.title}</Link></td>
             <td>{course.authorId}</td>
